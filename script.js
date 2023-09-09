15de680e980283b4e971aea73ab5b4ad1ba907ab
Nishasaw <139792337+Nishasaw@users.noreply.github.com>
2023-09-09 15:33:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function fibonacci(num) {
 // your code here
-	prev=0;
-	prev1=1;
-	num=prev+prev1;
-	num=prev1;
-	prev=prev1;
+	if(num===0)
+	{
+		return 0;
+	}
+	
+	else if(num===1)
+	{
+		return 1;
+	}
+	else
+    {
+		let fib=[0 , 1];
+		for (let i = 2; i <= num; i++) {
+		fib.push(fib[i-1]+fib[i-2]);
+		}
+		return fib[num]
+	}
+		
 }
-return num;
+console.log(fibonacci(1)); // Output: 0
+console.log(fibonacci(5)); // Output: 3
 
 
 module.exports = fibonacci;
